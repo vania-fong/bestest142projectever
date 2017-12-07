0b4b17d48e161f9a3feb20c6e07340dadb391285
vania-fong <fongvania@gmail.com>
2017-12-06 16:50:16 -0800

    Add files via upload

--- project_code_vf.R
+++ project_code_vf.R
@@ -22,6 +22,7 @@ library(rgeos)
 library(sp)
 library(rgdal)
 library(ggmap)
+library(data.table)
 
 
 #shapefile reading
@@ -41,13 +42,6 @@ convert_join <- function(df) {
   joined$TAZ = as.numeric(joined$TAZ)
   return(list(TransfSp = sp.transf, Joined = joined))
 }
-
-joined = convert_join(coord.rand)
-
-#overlay plots
-plot(joined[[1]], col = 'darkgreen',add= TRUE,  pch= 13, axes = TRUE)
-plot(taz.sf, axes = TRUE, border = 'lightgray')
-
 #calculate sf centroids
 sfcentroids = coordinates(taz.sf)
 centroid2taz = cbind(taz.sf@data, sfcentroids)
@@ -58,9 +52,13 @@ centroid2taz$TAZ = as.numeric(centroid2taz$TAZ)
 #random gps points
 rando = read_excel('Random-SF-Locations.xlsx') %>% 
   transform(geoloc=paste(Latitude, Longitude, sep="+"))
-key = "AIzaSyCtM0KUEC1PiENc5ivhEPSo9PPoKLRJ3Hk"
+key = 'AIzaSyCzHKDj9bsmhkS3o300TL9VET0XwywQJjs'	
+
+#ieor142 key
+#key = "AIzaSyCtM0KUEC1PiENc5ivhEPSo9PPoKLRJ3Hk"
 coord.rand = rando
 #coordinates(coord.rand) = ~Latitude + Longitude 
+
 
 
 #select popular TAZs
@@ -73,19 +71,21 @@ pop.rideshare = rideshare %>%
   mutate(rides = pickups + dropoffs) %>% 
   summarise(tot.rides = sum(rides)) %>% 
   arrange(desc(tot.rides)) %>% 
-  head(50)
+  head(20)
 
 #sort day + hour combinations by popularity
 time.rideshare = pop.rideshare   %>% 
   left_join(rideshare) %>% 
   left_join(centroid2taz, by = c('taz' = 'TAZ')) %>% 
   group_by(day_of_week, hour) %>% 
-  mutate(combined.rides = pickups + dropoffs) %>% 
+  mutate(combined.rides = pickups + dropoffs, hour.new = hour-3) %>% 
   arrange(desc(combined.rides)) %>% 
-  transform(geoloc=paste(Latitude, Longitude, sep="+")) %>% 
-  select(-c(taz, tot.rides), taz)
+  transform(geoloc=paste(Latitude, Longitude, sep="+"), DayHour = paste(day_of_week, hour, sep="D")) %>% 
+  select(-c(taz, tot.rides, hour), taz)
+
 View(time.rideshare)
 summary(time.rideshare$combined.rides)
+ggplot(time.rideshare, aes(DayHour, combined.rides)) + geom_col() + ylab('Total Rides Per Day/Hour Combination')
 
 #popular hours
 pop.hours = time.rideshare %>% 
@@ -101,25 +101,69 @@ ggplot(pop.hours, aes(hour.new, combined.rides)) + geom_col(fill = 'lightblue') 
 pop.days = time.rideshare %>% 
   group_by(day_of_week) %>% 
   summarise(combined.rides= mean(combined.rides))
-ggplot(pop.days, aes(day_of_week, combined.rides)) + geom_col(fill = 'pink') + geom_text(aes(label = round(combined.rides))) +
+ggplot(pop.days, aes(day_of_week, combined.rides)) + geom_col(fill = 'orange') + geom_text(aes(label = round(combined.rides))) +
   ylab('Average # of Rides During Each Day of Week Per TAZ') + xlab('Day of Week')
 
 
+joined = convert_join(time.rideshare)
+
+#overlay plots
+plot(joined[[1]], col = 'darkgreen',add= TRUE,  pch= 13, axes = TRUE)
+plot(taz.sf, axes = TRUE, border = 'lightgray')
+
+
 ##EDIT DEP TIME
-time_vec = as.POSIXct(format(as.POSIXct('2017-12-08 20:00'), tz = "GMT"))
+select.times = c('2017-12-17 03:00', '2017-12-17 09:00', '2017-12-17 11:00', '2017-12-17 19:00',
+                 '2017-12-18 03:00', '2017-12-18 09:00', '2017-12-18 11:00', '2017-12-18 19:00',
+                 '2017-12-22 03:00', '2017-12-22 09:00', '2017-12-22 11:00', '2017-12-22 19:00',
+                 '2017-12-23 03:00', '2017-12-23 09:00', '2017-12-23 11:00', '2017-12-23 19:00')                 
+time_vec = as.POSIXct(format(as.POSIXct(select.times), tz = "GMT"))
 time_vec.int = as.integer(time_vec)
 # format(as.POSIXct(gmt, tz = 'GMT'), tz = 'PST8PDT')
+time.car = list()
+time.car.list  = list()
 
-carmat = gmapsdistance(time.rideshare$geoloc[1:2], time.rideshare$geoloc[1:2], mode = 'driving', shape = 'long',
-                       ,departure = time_vec.int[1])
-carmat[[1]]= carmat[[1]] %>% 
-  cbind(Dep_Time.pst  = format(as.POSIXct(as.character(time_vec[1]), tz = 'GMT'), tz = 'PST8PDT'))
+#run to find trip duration for different 
+for (a in c(10, 20)){
+  
+  for (i in 1:16){
+    carmat = gmapsdistance(time.rideshare$geoloc[a-9:a], time.rideshare$geoloc[a-9:a], mode = 'driving', shape = 'long',
+                           ,key  = key, departure = time_vec.int[i])
+    time.car[[i]]= carmat[[1]] %>% 
+      cbind(Dep_Time.pst  = format(as.POSIXct(as.character(time_vec[i]), tz = 'GMT'), tz = 'PST8PDT'))
+    print(i)
+    Sys.sleep(3)
+  }
+  time.car.list[[a/10]] = time.car
+  print(a)
+  
+}
+#make a large dataframe with all car time
+time.car.final = rbindlist(time.car.list[[2]])
+write.csv(time.car.final, 'car1120.csv')
+
+time.transit = list()
+time.transit.list = list()
+for (a in c(10,20)){
+  for (j in 1:16){
+    transitmat = gmapsdistance(time.rideshare$geoloc[a-9:a], time.rideshare$geoloc[a-9:a], key = key,
+                               shape = 'long', mode = 'transit', departure = time_vec.int[j])
+    time.transit[[j]]= transitmat[[1]] %>% 
+      cbind(Dep_Time.pst  = format(as.POSIXct(as.character(time_vec[j]), tz = 'GMT'), tz = 'PST8PDT'))
+    Sys.sleep(3)
+    print(j)
+  }
+  time.transit.list[[a/10]] = time.transit[[a/10]]
+  print(a)
+}
 
 
-transitmat = gmapsdistance(time.rideshare$geoloc[1:2], time.rideshare$geoloc[1:2], key = key,
-                           shape = 'long', mode = 'transit', departure = time_vec.int[1])
-transitmat[[1]]= transitmat[[1]] %>% 
-  cbind(Dep_Time.pst  = format(as.POSIXct(as.character(time_vec[1]), tz = 'GMT'), tz = 'PST8PDT'))
+#make a large dataframe with all transit time
+time.transit.List1 = time.transit #has all times for geoloc[1:10]
+
+time.transit.final = rbindlist(time.transit.List)
+write.csv(rbindlist(time.transit.List1), 'transit110.csv')
+
 
 #calculate delta
 df = carmat[[1]] %>% 
@@ -149,3 +193,15 @@ gridded(Orig) = TRUE
 
 Dest = all.table[, grepl( "(Dest.*)" , names( all.table ) ) ]
 coordinates(Dest) <- ~ Dest.Lat + Dest.Lon
+
+
+
+
+
+
+
+# ###SAMPLE RBIND
+# DT1 = data.frame(A=1:3,B=letters[1:3])
+# DT2 = data.frame(B=letters[4:5],A=4:5)
+# l = list(DT1,DT2)
+# rbindlist(l)
